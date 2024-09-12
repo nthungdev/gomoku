2cc36900a77ea605dc47f4e1f2807d79bf4c193d
Hung Nguyen <nthungdev@gmail.com>
2024-09-12 02:26:47 -0400

    engine: checkWin

--- engine/src/engine.ts
+++ engine/src/engine.ts
@@ -3,9 +3,10 @@ import Player from './player'
 
 interface EngineConfig {
   boardSize: {
-    width: number;
-    height: number
+    rows: number
+    columns: number
   }
+  winLength: number
 }
 
 export default class Engine {
@@ -23,9 +24,9 @@ export default class Engine {
     this.config = config
   }
 
-  public init() {
+  private init() {
     const { boardSize } = this.config
-    this.board.init(boardSize.width, boardSize.height)
+    this.board.init(boardSize.rows, boardSize.columns)
     this.playerA.init()
     this.playerB.init()
   }
@@ -34,19 +35,8 @@ export default class Engine {
     this.init()
   }
 
-  public restart() {
-    this.init()
-  }
-
   public getTurnPlayer() {
     return this.turnPlayer
-  }
-
-  /**
-   * @returns the player who wins the game, else null
-   */
-  public checkWin() {
-
   }
 
   public play(player: Player, x: number, y: number) {
@@ -56,5 +46,78 @@ export default class Engine {
     // TODO update the board
     // TODO update the turn player
     // TODO check if the player wins
+  }
+
+  public updateBoard(newBoard: Board) {
+    this.board = newBoard
+  }
+
+  /**
+   * @returns the player who wins the game, else null
+   */
+  public checkWin() {
+    let winner = null
+    const winLength = 5
+
+    // group the picks by player
+    const playerPicks: Record<number, number[][]> = {}
+    for (let row = 0; row < this.config.boardSize.rows; row++) {
+      for (let column = 0; column < this.config.boardSize.columns; column++) {
+        const value = this.board.getState()[row][column]
+        if (value === 0) {
+          continue
+        }
+        if (!playerPicks[value]) {
+          playerPicks[value] = []
+        }
+        playerPicks[value].push([row, column])
+      }
+    }
+
+    for (const player in playerPicks) {
+      const picks = playerPicks[player]
+      if (picks.length < winLength) {
+        continue
+      }
+      for (let i = 0; i < picks.length - 4; i++) {
+        const [x, y] = picks[i]
+        const restPicks = picks.slice(i + 1)
+        if (this.checkLine(x, y, restPicks, 'horizontal')) {
+          winner = player
+        } else if (this.checkLine(x, y, restPicks, 'vertical')) {
+          winner = player
+        } else if (this.checkLine(x, y, restPicks, 'diagonal')) {
+          winner = player
+        }
+      }
+    }
+    return { winner }
+  }
+
+  private checkLine(
+    x: number,
+    y: number,
+    picks: number[][],
+    direction: 'horizontal' | 'vertical' | 'diagonal'
+  ) {
+    const winningPicks = [[x, y]]
+    let streakCount = 1
+    const streakMap = {
+      horizontal: [0, 1],
+      vertical: [1, 0],
+      diagonal: [1, 1],
+    }
+    const [dx, dy] = streakMap[direction]
+    for (const pick of picks) {
+      const [x1, y1] = pick
+      if (x1 === x + dx * streakCount && y1 === y + dy * streakCount) {
+        streakCount++
+        winningPicks.push([x1, y1])
+        if (streakCount === this.config.winLength) {
+          return true
+        }
+      }
+    }
+    return false
   }
 }
