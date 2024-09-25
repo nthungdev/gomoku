238e8632a9844dbdffdb8602afb3a0074fa66f8c
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2024-09-25 13:02:27 -0400

    CLI interface (#7)
    * rename shared to common
    
    * setup yarn workspace
    
    * update package name
    
    * configure engine build
    
    * import engine from workspace in cli
    
    * update cli scripts
    
    * player name
    
    * update workspace
    
    * working CLI
    
    * improve play name prompt
    
    * nicer board print
    
    * update move prompt

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,3 +1,118 @@
-import figlet from "figlet";
+import figlet from 'figlet'
+import { createInterface } from 'readline'
+import * as process from 'process'
+import { Engine, Player } from '@gomoku/engine'
 
-console.log(figlet.textSync("Dir Manager"));
+const PLAYER_1_VALUE = 1
+const PLAYER_2_VALUE = 2
+const PLAYER_1_SYMBOL = '✖'
+const PLAYER_2_SYMBOL = '◯'
+
+const cellToSymbol: Record<number, string> = {
+  0: '.',
+  [PLAYER_1_VALUE]: PLAYER_1_SYMBOL,
+  [PLAYER_2_VALUE]: PLAYER_2_SYMBOL,
+}
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+/**
+ * @param input string contains "row column"
+ * @returns null if failed to parse, else { row: number, column: number }
+ */
+const parseMoveInput = (input: string) => {
+  const [row, column] = input.trim().split(' ')
+  const parsedRow = parseInt(row)
+  const parsedColumn = parseInt(column)
+  if (isNaN(parsedRow) || isNaN(parsedColumn)) {
+    return null
+  }
+  return {
+    row: parsedRow,
+    column: parsedColumn,
+  }
+}
+
+const printBoardState = (boardState: number[][]) => {
+  const boardString = boardState
+    .map((row) => row.map((cell) => cellToSymbol[cell]).join(' '))
+    .join('\n')
+  console.log(boardString)
+}
+
+const loop = (engine: Engine) => {
+  console.log('_'.repeat(20))
+  printBoardState(engine.getBoardState())
+
+  const turnPlayer = engine.getTurnPlayer()
+  readline.question(
+    `${turnPlayer}: Enter your move ${cellToSymbol[turnPlayer.value]} (row column): `,
+    (input) => {
+      const move = parseMoveInput(input)
+      if (!move) {
+        console.log('Invalid input, please try again')
+        return loop(engine)
+      }
+      const { row, column } = move
+
+      try {
+        const { winner } = engine.play(turnPlayer, row, column)
+        if (winner) {
+          console.log(`${winner} wins!`)
+          printBoardState(engine.getBoardState())
+          process.exit(0)
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message)
+        } else {
+          console.log('An error occurred')
+        }
+        return loop(engine)
+      }
+
+      return loop(engine)
+    }
+  )
+}
+
+const promptName = async (prompt: string, takenName?: string) => {
+  const name = (
+    await new Promise<string>((resolve) => {
+      readline.question(prompt, resolve)
+    })
+  ).trim()
+  if (name === '') {
+    console.log('Name cannot be empty')
+    return promptName(prompt, takenName)
+  } else if (name === takenName) {
+    console.log('Name already taken')
+    return promptName(prompt, takenName)
+  }
+  return name
+}
+
+const game = async () => {
+  console.log(figlet.textSync('Gomoku'))
+
+  const name1 = await promptName("What is player 1's name? ")
+  const name2 = await promptName("What is player 2's name? ", name1)
+  const players: Player[] = [
+    new Player({ value: PLAYER_1_VALUE, name: name1.trim() }),
+    new Player({ value: PLAYER_2_VALUE, name: name2.trim() }),
+  ]
+
+  const engine = new Engine({
+    boardSize: { rows: 10, columns: 10 },
+    winLength: 5,
+    players,
+    firstPlayer: players[0],
+  })
+  engine.start()
+  loop(engine)
+}
+
+game()
--- engine/src/player.ts
+++ engine/src/player.ts
@@ -1,11 +1,18 @@
 interface PlayerConfig {
   value: number
+  name?: string
 }
 
 export default class Player {
   public value: number;
+  public name: string;
 
   constructor(config: PlayerConfig) {
     this.value = config.value;
+    this.name = config.name || `Player ${config.value}`;
+  }
+
+  toString(): string {
+    return this.name;
   }
 }
