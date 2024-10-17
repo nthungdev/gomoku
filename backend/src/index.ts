8b371310d3adac875232f74d806283510443769b
Hung Nguyen <nthungdev@gmail.com>
2024-10-17 05:27:21 -0400

    run backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,16 @@
-import express, { Express } from 'express'
 import dotenv from 'dotenv'
 import { Server, createServer } from 'http'
-import cors from 'cors'
+import app from './app'
 
 dotenv.config()
 
-const app: Express = express()
-app.use(cors())
-
-const httpServer: Server = createServer(app)
 const port = process.env.PORT || 8000
+const server: Server = createServer(app)
 
 app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server!')
+  res.send('Gomoku Server!')
 })
 
-httpServer.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`)
 })
