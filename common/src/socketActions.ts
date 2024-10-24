ef53e345e245450489f28414668bb7bad8408758
Hung Nguyen <nthungdev@gmail.com>
2024-10-23 21:37:23 -0400

    common socket events and actions

---
+++ common/src/socketActions.ts
@@ -0,0 +1,7 @@
+export const ACTION_JOIN_ROOM = 'action:joinRoom'
+export const ACTION_LEAVE_ROOM = 'action:leaveRoom'
+export const ACTION_CREATE_ROOM = 'action:createRoom'
+
+export const ACTION_MAKE_MOVE = 'action:makeMove'
+export const ACTION_START_GAME = 'action:startGame'
+export const ACTION_SURRENDER = 'action:surrender'
---
+++ common/src/socketEvents.ts
@@ -0,0 +1,7 @@
+export const EVENT_USER_JOINED_ROOM = 'event:userJoinedRoom'
+export const EVENT_USER_LEFT_ROOM = 'event:userLeftRoom'
+export const EVENT_USER_MADE_MOVE = 'event:userMadeMove'
+export const EVENT_USER_SURRENDERED = 'event:userSurrendered'
+
+export const EVENT_GAME_STARTED = 'event:gameStarted'
+export const EVENT_GAME_OVER = 'event:gameOver'
