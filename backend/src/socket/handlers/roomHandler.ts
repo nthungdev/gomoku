c97042e5bb7fb6d4d641f07a40bca177962588ec
Hung Nguyen <nthungdev@gmail.com>
2024-10-24 01:27:32 -0400

    update roomHandler

--- backend/src/socket/handlers/roomHandler.ts
+++ backend/src/socket/handlers/roomHandler.ts
@@ -1,38 +1,90 @@
+import { User } from '@/db/types'
 import { EVENT_USER_JOINED_ROOM, EVENT_USER_LEFT_ROOM } from '@gomoku/common'
+import { Player } from '@gomoku/engine'
 import { Server, Socket } from 'socket.io'
+
+const PLAYER_1_VALUE = 1
+const PLAYER_2_VALUE = 2
+const PLAYER_1_NAME = 'Player 1'
+const PLAYER_2_NAME = 'Player 2'
 
 export default function roomHandler(io: Server, socket: Socket) {
   const userId = socket.id
 
   async function createRoom() {
-    // TODO make sure the user hasn't created a room already
+    // make sure the user hasn't created a room already
+    const room = await socket.db.getRoomHasUser(userId)
+    if (room) {
+      // TODO send error message
+      return
+    }
 
-    const roomId = await socket.db.createRoom()
+    const user = {
+      id: userId,
+      player: new Player({
+        value: PLAYER_1_VALUE,
+        name: PLAYER_1_NAME,
+      }),
+    }
+    const roomId = await socket.db.createRoom(user)
 
-    io.to(roomId).emit(EVENT_USER_JOINED_ROOM, { userId: userId })
+    await joinRoom(roomId)
   }
 
-  // async function joinRoom(roomId: string) {
   async function joinRoom(roomId: string) {
-    // TODO validate room exists
-    // TODO validate room isn't full
+    // validate room exists
+    const room = await socket.db.getRoomById(roomId)
+    if (!room) {
+      // TODO send error message
+      return
+    }
 
-    await socket.db.addUserToRoom(roomId, userId)
+    // validate room isn't full
+    if (room.users.length >= 2) {
+      // TODO send error message
+      return
+    }
+
+    const user: User = {
+      id: userId,
+      player: new Player({
+        value: PLAYER_2_VALUE,
+        name: PLAYER_2_NAME,
+      }),
+    }
+
+    await socket.db.addUserToRoom(roomId, user)
 
     socket.join(roomId)
 
-    io.to(roomId).emit(EVENT_USER_JOINED_ROOM, { userId: userId })
+    io.to(roomId).emit(EVENT_USER_JOINED_ROOM, { userId })
   }
 
-
   async function leaveRoom(roomId: string) {
-    // TODO validate roomId
+    // validate room exists
+    const room = await socket.db.getRoomById(roomId)
+    if (!room) {
+      // TODO send error message
+      return
+    }
+
+    // validate user is in the room
+    if (!room.users.find((user) => user.id === userId)) {
+      // TODO send error message
+      return
+    }
+
+    await socket.db.removeUserFromRoom(roomId, userId)
 
     socket.leave(roomId)
 
     io.to(roomId).emit(EVENT_USER_LEFT_ROOM, { userId })
 
     // TODO delete room if empty
+
+    if (room.users.length === 1) {
+      await socket.db.removeRoom(roomId)
+    }
   }
 
   return {
