235325a7520a23bd25221113b6f917bd1fa70181
Hung Nguyen <nthungdev@gmail.com>
2024-11-07 03:35:36 -0500

    socket callback

--- backend/src/socket/handlers/gameHandler.ts
+++ backend/src/socket/handlers/gameHandler.ts
@@ -1,5 +1,4 @@
 import {
-  ACTION_MAKE_MOVE,
   EVENT_GAME_OVER,
   EVENT_GAME_STARTED,
   EVENT_USER_MADE_MOVE,
@@ -8,21 +7,27 @@ import {
 import { Engine, GameMove } from '@gomoku/engine'
 import { Server, Socket } from 'socket.io'
 import { getGameStateFromRoom } from '../utils'
+import { SocketCallback } from '../types'
+import { SocketErrorMessage } from '../error'
 
 export default function gameHandler(io: Server, socket: Socket) {
   const userId = socket.id
 
-  async function makeMove(roomId: string, move: GameMove) {
+  async function makeMove(
+    roomId: string,
+    move: GameMove,
+    callback?: SocketCallback
+  ) {
     if (move.type === 'move') {
       const room = await socket.db.getRoomById(roomId)
       if (!room) {
-        // TODO send error message
+        callback?.({ error: SocketErrorMessage.RoomNotFound })
         return
       }
 
       const player = room.users.find((user) => user.id === userId)?.player
       if (!player) {
-        // TODO send error message
+        callback?.({ error: SocketErrorMessage.PlayerNotFound })
         return
       }
       // TODO handle invalid move error thrown by engine
@@ -33,26 +38,27 @@ export default function gameHandler(io: Server, socket: Socket) {
       } else {
         io.to(roomId).emit(EVENT_USER_MADE_MOVE, gameState)
       }
+      callback?.({ ok: true })
     } else if (move.type === 'surrender') {
       surrender(roomId)
       return
     } else {
-      // TODO send error message in valid move type
+      callback?.({ error: SocketErrorMessage.InvalidMove })
       return
     }
   }
 
-  async function surrender(roomId: string) {
+  async function surrender(roomId: string, callback?: SocketCallback) {
     // validate the user is in the room
     const room = await socket.db.getRoomById(roomId)
     if (!room) {
-      // TODO send error message
+      callback?.({ error: SocketErrorMessage.RoomNotFound })
       return
     }
 
     const player = room.users.find((user) => user.id === userId)?.player
     if (!player) {
-      // TODO send error message
+      callback?.({ error: SocketErrorMessage.PlayerNotFound })
       return
     }
 
@@ -63,13 +69,14 @@ export default function gameHandler(io: Server, socket: Socket) {
 
     io.to(roomId).emit(EVENT_USER_SURRENDERED, gameState)
     io.to(roomId).emit(EVENT_GAME_OVER, gameState)
+    callback?.({ ok: true })
   }
 
-  async function startGame(roomId: string) {
+  async function startGame(roomId: string, callback?: SocketCallback) {
     // validate this user is the room owner
     const room = await socket.db.getRoomById(roomId)
     if (room?.owner.id !== userId) {
-      // TODO send error message
+      callback?.({ error: SocketErrorMessage.PlayerNotRoomOwner })
       return
     }
 
@@ -90,6 +97,7 @@ export default function gameHandler(io: Server, socket: Socket) {
     const gameState = getGameStateFromRoom(room)
 
     io.to(roomId).emit(EVENT_GAME_STARTED, gameState)
+    callback?.({ ok: true })
   }
 
   return {
