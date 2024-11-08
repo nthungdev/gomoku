63372c0e30cab48d96171ae02edc47484879a13f
Hung Nguyen <nthungdev@gmail.com>
2024-11-07 19:54:07 -0500

    BasePage extends div props

--- web/src/components/BasePage.tsx
+++ web/src/components/BasePage.tsx
@@ -1,3 +1,18 @@
-export default function BasePage({ children }: { children: React.ReactNode }) {
-  return <div className="w-full max-w-screen-lg mx-auto py-4 px-2">{children}</div>
+import classNames from 'classnames'
+import { ComponentProps } from 'react'
+
+export default function BasePage({
+  children,
+  className,
+}: ComponentProps<'div'>) {
+  return (
+    <div
+      className={classNames(
+        'flex-1 w-full max-w-screen-lg mx-auto py-4 px-2',
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
 }
