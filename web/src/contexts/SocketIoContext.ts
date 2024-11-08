f0c3d03168ace16ffb1b515e5b0aea056934afa3
Hung Nguyen <nthungdev@gmail.com>
2024-11-07 19:36:17 -0500

    update socket provider value

--- web/src/contexts/SocketIoContext.ts
+++ web/src/contexts/SocketIoContext.ts
@@ -1,10 +1,22 @@
 import { createContext, useContext } from 'react'
 
-interface SocketIoContext {
+interface SocketIoContextBase {
   isConnected: boolean
   joinRoom: (roomId: string) => void
   createRoom: () => void
 }
+
+interface ConnectedSocketIoContext extends SocketIoContextBase {
+  isConnected: true
+  id: string
+}
+
+interface DisconnectedSocketIoContext extends SocketIoContextBase {
+  isConnected: false
+  id?: undefined
+}
+
+type SocketIoContext = ConnectedSocketIoContext | DisconnectedSocketIoContext
 
 export const SocketIoContext = createContext<SocketIoContext>(
   // proper properties are set in SocketIoProvider
