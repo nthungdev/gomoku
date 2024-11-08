af6cea9f35abfbf6055cc7652250f711c3a3831e
Hung Nguyen <nthungdev@gmail.com>
2024-11-07 19:36:46 -0500

    Header

--- web/src/routes/Root.tsx
+++ web/src/routes/Root.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
 
 function Root() {
-
-  return <div className="flex flex-col w-full min-h-screen">
-    <Outlet />
-  </div>
+  return (
+    <div className="flex flex-col w-full min-h-screen">
+      <Header />
+      <Outlet />
+    </div>
+  )
 }
 
 export default Root
