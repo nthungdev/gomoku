b910fd40f2b17f02ab5f2b2c6472645d3e8d3cb1
Hung Nguyen <nthungdev@gmail.com>
2024-11-14 14:36:45 -0500

    setup web env var

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 // "undefined" means the URL will be computed from the `window.location` object
 const SOCKET_URL =
-  process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8000'
+  process.env.NODE_ENV === 'production' ? undefined : import.meta.env.VITE_SOCKET_URL
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
